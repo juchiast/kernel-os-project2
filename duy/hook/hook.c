b2e1555a774c6e2f7b6d5a833a9e02289d958bd3
Do Duy <juchiast@gmail.com>
2018-12-05 18:52:49 +0700

    finish

--- duy/hook/hook.c
+++ duy/hook/hook.c
@@ -28,7 +28,7 @@ static char* pidtoname(int pid) {
 }
 
 static char *get_path(struct files_struct *files, unsigned int fd) {
-    char *pathname;
+    char *pathname, *ret, *tmp;
     struct file *file;
     struct path *path;
     spin_lock(&files->file_lock);
@@ -45,33 +45,50 @@ static char *get_path(struct files_struct *files, unsigned int fd) {
         path_put(path);
         return NULL;
     }
-    if (IS_ERR(d_path(path, pathname, PAGE_SIZE))) {
-        path_put(path);
+    ret = d_path(path, pathname, 1024);
+    path_put(path);
+    if (IS_ERR(ret)) {
         kfree(pathname);
         return NULL;
     }
-    path_put(path);
-    return pathname;
+    tmp = kmalloc(1024, GFP_KERNEL);
+    if (tmp == NULL) {
+        kfree(pathname);
+        return NULL;
+    }
+    strncpy(tmp, ret, 1024);
+    kfree(pathname);
+    return tmp;
 }
 
 static long (*origin_open)(struct pt_regs *regs);
 static long (*origin_write)(struct pt_regs *regs);
 
 static long my_open(struct pt_regs *regs) {
+    const char __user *fname = (const char __user *)regs->di;
     long ret = origin_open(regs);
     char *name = pidtoname(task_pid_nr(current));
     if (strcmp(name, "dmesg") != 0) {
-        printk(KERN_INFO "process %s open", name);
+        char *kfname = kmalloc(1024, GFP_KERNEL);
+        strncpy(kfname, fname, 1024);
+        printk(KERN_INFO "process %s open %s", name, kfname);
     }
     return ret;
 }
 static long my_write(struct pt_regs *regs) {
+    unsigned int fd = (unsigned int)regs->di;
+    size_t count = (size_t)regs->dx;
     long ret = origin_write(regs);
     char *name = pidtoname(task_pid_nr(current));
     if (strcmp(name, "dmesg") != 0) {
-        printk(KERN_INFO "process %s write", name);
+        char *path = get_path(current->files, fd);
+        if (path != NULL) {
+            printk(KERN_INFO "process %s write %lu bytes to %s", name, count, path);
+            kfree(path);
+        } else {
+            printk(KERN_INFO "process %s write %lu bytes to %u", name, count, fd);
+        }
     }
-
     return ret;
 }
 
